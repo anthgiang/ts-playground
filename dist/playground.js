73323d23a7b6d9635cc1e0d69c7a4fb640f7ff9f
Anthony Giang <anth@sent.com>
2022-11-05 21:20:20 +1100

    Rename to playground

--- dist/playground.js
+++ dist/playground.js
@@ -2,9 +2,9 @@
 function printCar(car) {
     let str = `${car.make} ${car.model} (${car.year})`;
     car.chargeVoltage;
-    // if (typeof car.chargeVoltage !== "undefined") {
-    str += `// ${car.chargeVoltage.toFixed(2)}v`;
-    // }
+    if (typeof car.chargeVoltage !== "undefined") {
+        str += `// ${car.chargeVoltage.toFixed(2)}v`;
+    }
     console.log(str);
 }
 // Works
--- src/playground.ts
+++ src/playground.ts
@@ -1,9 +1,9 @@
 function printCar(car: { make: string; model: string; year: number; chargeVoltage?: number }): void {
   let str = `${car.make} ${car.model} (${car.year})`;
   car.chargeVoltage;
-  // if (typeof car.chargeVoltage !== "undefined") {
-  str += `// ${car.chargeVoltage.toFixed(2)}v`;
-  // }
+  if (typeof car.chargeVoltage !== "undefined") {
+    str += `// ${car.chargeVoltage.toFixed(2)}v`;
+  }
   console.log(str);
 }
 // Works
